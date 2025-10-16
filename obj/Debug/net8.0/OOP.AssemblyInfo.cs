279fc105ee19fd7614fc88936c061a41ca14125c
meaxxng <meaxxng.1073@gmail.com>
2025-10-16 14:47:41 +0700

    new_

--- obj/Debug/net8.0/OOP.AssemblyInfo.cs
+++ obj/Debug/net8.0/OOP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a9bf604544ec9c1d1e1b2fe294453efe016ad3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+966e50479f6ade7b2938a367451924554ef712a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
